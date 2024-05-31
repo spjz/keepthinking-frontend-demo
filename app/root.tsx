a1a480d1fba828598ddb35bdd0b69e7c5f8ce0a8
Joe Doherty <papaverjosepherum@gmail.com>
2024-05-31 13:33:00 +0100

    Converted to React-Remix app

--- app/root.tsx
+++ app/root.tsx
@@ -6,16 +6,20 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import { LinksFunction } from "@remix-run/node";
+import styles from "./tailwind.css?url";
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
+
+      <body className="h-full w-full">
         {children}
         <ScrollRestoration />
         <Scripts />
@@ -23,6 +27,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
     </html>
   );
 }
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+];
 
 export default function App() {
   return <Outlet />;
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,32 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Header } from "~/components/header";
+import { Footer } from "~/components/footer";
+import { Artists } from "~/components/artists";
+import { Gallery } from "~/components/gallery";
+import { Events } from "~/components/events";
+import { Channel } from "~/components/channel";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Keepthinking Front-end Demo" },
+    { name: "description", content: "Keepthinking front-end demo, using Remix, Tailwindcss" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="flex flex-col items-center">
+
+      <Header title="Art Gallery" />
+
+      <main className="w-8/12 px-6">
+        <Gallery />
+        <Events />
+        <Channel />
+        <Artists />
+      </main>
+
+      <Footer />
     </div>
   );
 }
